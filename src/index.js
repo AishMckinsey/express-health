6d4f84e5f4ec8266d755cb1c204be5c58f6b17b4
Aishwarya N <Aishwarya_N@mckinsey.com>
2022-04-01 00:18:21 +0530

    feat: add example endpoint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const env = require("dotenv");
 
 const { healthRouter } = require("./routes/health.routes");
+const { exampleRouter } = require("./routes/example.routes");
 
 env.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use("/health", healthRouter);
+app.use("/example", exampleRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
